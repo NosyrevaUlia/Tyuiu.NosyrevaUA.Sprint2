32da922bd7d5ad7b989479d97fbdaec20348f195
Tyuiu.NosyrevaUA@outlook.com <ulianosyreva752@gmail.com>
2023-10-02 19:34:18 +0500

    Task0

---
+++ Tyuiu.NosyrevaUA.Sprint2.Task0.V13.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.NosyrevaUA.Sprint2.Task0.V13.Lib;
+
+namespace Tyuiu.NosyrevaUA.Sprint2.Task0.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCompareOperations()
+        {
+            DataService ds = new DataService();
+            int x = 1905;
+            int y = 475;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] w = new bool[6] { false, true, false, true, false, true };
+            CollectionAssert.AreEqual(w, res);
+
+        }
+    }
+}
---
+++ Tyuiu.NosyrevaUA.Sprint2.Task0.V13.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NosyrevaUA.Sprint2.Task0.V13.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NosyrevaUA.Sprint2.Task0.V13.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("63557c93-b972-4614-9b9e-9a2ee8040af0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
